1b0d3aca5fdd2bcf7533b80379029cd3a7b3b8d6
Vitor Felde <feldevitor@gmail.com>
2026-05-11 10:32:36 -0300

    Delete atividade 9 - JS (Funções e eventos)/eventos.js

--- atividade 9 - JS (Funções e eventos)/eventos.js
+++
@@ -1,6 +0,0 @@
-function alteraCor (elemento, cor) { //aqui, estao separados por virgula, e os valores sao definidos dentro
-    //altera a cor de background do elemento (div)
-    elemento.style.backgroundColor = cor; //puxando a cor, que foi definida no html, e atribuindo-a o valor de elemento que é a div
-}
-alteraCor(); //retornando funcao
-
